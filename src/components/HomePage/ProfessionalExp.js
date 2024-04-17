34d820502f4dc87ea4595f5059b26922f5d7ceac
mfonPeeter <godimfon@gmail.com>
2024-04-17 18:13:37 +0100

    redesigned the Professional and Voluntary experience section

--- src/components/HomePage/ProfessionalExp.js
+++ src/components/HomePage/ProfessionalExp.js
@@ -1,6 +1,8 @@
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+
 const ProfessionalExp = () => {
   return (
-    <section id="professional" className="pt-10 pb-16">
+    <section id="professional" className="pt-16 pb-16 bg-[#fdf2e9]">
       <div className="px-2 mb-12 text-center lg:mb-16">
         <h2>Professional Experience</h2>
         <p className="max-w-2xl px-4 mx-auto text-lg text-gray-800 sm:px-10 md:text-xl md:max-w-3xl md:px-0">
@@ -9,79 +11,206 @@ const ProfessionalExp = () => {
         </p>
       </div>
 
-      <div className="container mx-auto max-w-7xl px-4 mb-10 flex justify-around text-gray-800 sm:px-10 md:space-x-6 lg:space-x-0">
-        <div className="hidden text-lg font-semibold md:block">
-          <p>May 2023 - Oct 2023</p>
-          <p>United States | Remote</p>
-        </div>
-        <div>
-          <h3>Frontend Developer</h3>
-          <p className="text-lg font-semibold italic md:mb-2">GoAppiT</p>
-          <div className="font-semibold mb-2 md:hidden">
-            <p>May 2023 - Oct 2023</p>
-            <p>United States | Remote</p>
+      <div className="container mx-auto max-w-7xl grid grid-cols-1 gap-6 px-4 md:grid-cols-2">
+        <div className="flex flex-col p-6 bg-white rounded-3xl shadow-md transform transition-all duration-300 hover:scale-[1.01] hover:shadow-lg">
+          <div className="flex flex-col mb-4 font-semibold">
+            <div className="flex flex-col justify-between xs:flex-row">
+              <span className="text-xl">GOAPPiT</span>
+              <span className="font-normal">Oct 2023 - Jan 2024</span>
+            </div>
+            <span className="text-lg">Software Engineer</span>
+            <span>Fort Worth, Texas, United States</span>
           </div>
-          <p className="max-w-2xl text-lg mb-2">
-            • Redesigned the UI of a client's website and rewrote the code in
-            HTML and Tailwind CSS making it fully responsive with a functional
-            sidebar menu.
-          </p>
-          <p className="text-lg mb-2">
-            • Utilize ReactJS and Tailwind CSS to rebuild the frontend of the
-            company's website.
-          </p>
-          <p className="text-lg mb-2">
-            • Assisting with the UI/UX planning of the company's website
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Toolsets: </span>
-            Visual Studio Code, Git, HTML, ReactJS, Tailwind CSS
-          </p>
+          <ul className="flex flex-col space-y-2 mb-4">
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Utilized Flask to establish seamless connectivity with the
+                Cargowise system on behalf of a client.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Utilized Django and JavaScript to revamp and modernize the
+                database structure of an invoice form for a client,
+                incorporating removal of outdated fields, addition of new
+                fields, and modification of existing ones.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Developed a Python script utilizing Paramiko to establish SFTP
+                connectivity, enabling the secure retrieval of files from a
+                remote server to a local directory, along with the capability to
+                delete files remotely.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Leveraged Pandas to create a Python script for formatting an
+                Excel file into the desired layout.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Crafted a Python script to extract data from a directory of text
+                files, dynamically generating URLs for scraping data from
+                corresponding websites.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Engaged in the collaborative process of peer reviews by
+                submitting concise and focused pull requests, aimed at improving
+                code quality, upholding best practices, and fostering shared
+                learning among team members.
+              </span>
+            </li>
+          </ul>
+          <div className="grow shrink" />
+          <div className="grid grid-cols-2 gap-1 text-[14.5px] font-semibold xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-4 lg:flex">
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              JavaScript
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Python
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Flask
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Django
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Bootstrap
+            </span>
+          </div>
         </div>
-      </div>
 
-      <div className="container mx-auto max-w-7xl px-4 flex justify-around text-gray-800 sm:px-10 md:space-x-6 lg:space-x-0">
-        <div className="hidden text-lg font-semibold md:block">
-          <p>Oct 2023 - Jan 2024</p>
-          <p>United States | Remote</p>
-        </div>
-        <div>
-          <h3>Software Engineer</h3>
-          <p className="text-lg font-semibold italic md:mb-2">GoAppiT</p>
-          <div className="font-semibold mb-2 md:hidden">
-            <p>Oct 2023 - Jan 2024</p>
-            <p>United States | Remote</p>
+        <div className="flex flex-col p-6 bg-white rounded-3xl shadow-md transform transition-all duration-300 hover:scale-[1.01] hover:shadow-lg">
+          <div className="flex flex-col mb-4 font-semibold">
+            <div className="flex flex-col justify-between xs:flex-row">
+              <span className="text-xl">GOAPPiT</span>
+              <span className="font-normal">May 2023 - Oct 2023</span>
+            </div>
+            <span className="text-lg">Frontend Engineer</span>
+            <span>Fort Worth, Texas, United States</span>
           </div>
-          <p className="max-w-2xl text-lg mb-2">
-            • Utilize Flask to connect to Cargowise system for a client
-          </p>
-          <p className="max-w-2xl text-lg mb-2">
-            • Utilize Django and JavaScript to redesign and update the database
-            of an invoice form for a client by removing old fields, adding new
-            fields, or making changes to existing ones.
-          </p>
-          <p className="max-w-2xl text-lg mb-2">
-            • Utilize Django to restrict users' access to certain data unless
-            part of a certain group in the Django admin panel
-          </p>
-          <p className="max-w-2xl text-lg mb-2">
-            • Wrote a Python script to connect to an SFTP server to download
-            files to a local directory and delete files in the remote directory
-            using Paramiko
-          </p>
-          <p className="max-w-2xl text-lg mb-2">
-            • Utilize Pandas to write a Python script which formatted an Excel
-            file to the desired format.
-          </p>
-          <p className="max-w-2xl text-lg mb-2">
-            • Wrote a Python script to get data from a list of text files in a
-            directory to use the data gotten to create a dynamic URL to scrape
-            data from a website.
-          </p>
-          <p className="max-w-2xl text-lg">
-            <span className="font-semibold">Toolsets: </span>
-            Visual Studio Code, Git, JavaScript, Python, Django, Flask
-          </p>
+          <ul className="flex flex-col space-y-2 mb-4">
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Successfully redesigned the UI of a client's website,
+                implementing HTML and Tailwind to rewrite the code for full
+                responsiveness, and integrating a functional sidebar menu.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Demonstrated proficiency in employing React and Tailwind to
+                completely overhaul the frontend of the company's website.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Contributed valuable insights and support in the strategic
+                planning of UI/UX enhancements for the company's website.
+              </span>
+            </li>
+          </ul>
+          <div className="grow shrink" />
+          <div className="grid grid-cols-2 gap-1 text-[14.5px] font-semibold xs:flex">
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              JavaScript
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              React
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Tailwind CSS
+            </span>
+          </div>
+        </div>
+
+        <div className="flex flex-col p-6 bg-white rounded-3xl shadow-md transform transition-all duration-300 hover:scale-[1.01] hover:shadow-lg">
+          <div className="flex flex-col mb-4 font-semibold">
+            <div className="flex flex-col justify-between xs:flex-row">
+              <span className="text-xl">Dealdrive</span>
+              <span className="font-normal">April 2023 - May 2023</span>
+            </div>
+            <span className="text-lg">Frontend Developer</span>
+            <span>Nigeria</span>
+          </div>
+          <ul className="flex flex-col space-y-2 mb-4">
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Utilized ReactJS to architect dynamic and responsive frontend
+                experiences tailored to meet clients' unique requirements,
+                ensuring optimal performance and user engagement.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Engaged directly with the design team to analyze and optimize
+                user interface (UI) elements, ensuring seamless integration of
+                design aesthetics and functionality for clients' websites.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Implemented a WhatsApp widget into clients' websites, empowering
+                potential customers to initiate swift communication with the
+                client, and fostering faster and more direct engagement
+                opportunities.
+              </span>
+            </li>
+          </ul>
+          <div className="grow shrink" />
+          <div className="grid grid-cols-2 gap-1 text-[14.5px] font-semibold xs:flex">
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              JavaScript
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              React
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Tailwind CSS
+            </span>
+          </div>
         </div>
       </div>
     </section>
--- src/components/HomePage/VolunteerExp.js
+++ src/components/HomePage/VolunteerExp.js
@@ -1,6 +1,8 @@
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+
 const VolunteerExp = () => {
   return (
-    <section className="pt-10 pb-16">
+    <section className="pt-16 pb-16">
       <div className="px-2 mb-12 text-center lg:mb-16">
         <h2>Volunteer Experience</h2>
         <p className="max-w-2xl px-4 mx-auto text-lg text-gray-800 sm:px-10 md:text-xl md:max-w-3xl md:px-0">
@@ -8,30 +10,58 @@ const VolunteerExp = () => {
           build my skills and increase my experience level.
         </p>
       </div>
-
-      <div className="container mx-auto max-w-7xl px-4 flex justify-around text-gray-800 sm:px-10 md:space-x-6 lg:space-x-0">
-        <div className="hidden text-lg font-semibold md:block">
-          <p>March 2023 - April 2023</p>
-          <p>Uyo, Nigeria | Remote</p>
-        </div>
-        <div>
-          <h3>Frontend Developer</h3>
-          <p className="text-lg font-semibold italic md:mb-2">George's Work</p>
-          <div className="font-semibold mb-2 md:hidden">
-            <p>March 2023 - April 2023</p>
-            <p>Uyo, Nigeria | Remote</p>
+      <div className="container mx-auto max-w-7xl px-4">
+        <div className="flex flex-col p-6 bg-white rounded-3xl shadow-md border transform transition-all duration-300 hover:scale-[1.01] hover:shadow-lg">
+          <div className="flex flex-col mb-4 font-semibold">
+            <div className="flex flex-col justify-between xs:flex-row">
+              <span className="text-xl">George's Work</span>
+              <span className="font-normal">March 2023 - April 2023</span>
+            </div>
+            <span className="text-lg">Frontend Developer</span>
+            <span>Nigeria</span>
           </div>
-          <p className="max-w-2xl text-lg">
-            • I helped build the landing page and the different authentication
-            pages such as Login, Sign up, Forgot Password, etc.
-          </p>
-          <p className="text-lg mb-2">
-            • I made sure that the pages built were fully responsive.
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Tools Used: </span>
-            React, Tailwind CSS, Git, GitHub
-          </p>
+          <ul className="flex flex-col space-y-2 mb-4">
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Utilize ReactJS and Tailwind CSS to convert Figma mockup designs
+                into codes
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Created React components for the landing page, and
+                authentication pages such as Login, Sign Up, and Forgot
+                Password.
+              </span>
+            </li>
+            <li className="flex space-x-2">
+              <div className="mt-1">
+                <BsFillCheckCircleFill color="orange" size="15" />
+              </div>
+              <span>
+                Utilize Tailwind CSS to make all the pages responsive across
+                multiple screen sizes.
+              </span>
+            </li>
+          </ul>
+          <div className="grow shrink" />
+          <div className="grid grid-cols-2 gap-1 text-[14.5px] font-semibold xs:flex">
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              JavaScript
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              React
+            </span>
+            <span className="flex items-center justify-center px-3 py-0.5 text-orange-500 bg-orange-100 rounded-full">
+              Tailwind CSS
+            </span>
+          </div>
         </div>
       </div>
     </section>
